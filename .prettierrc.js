0a18ba3c9c4b8cda33ca926a5a8b01d6f54ab047
Jussi Saarela <jussi.saarela@lemonsoft.fi>
2022-03-07 14:45:42 +0200

    Fixed lint and build errors

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-  trailingComma: 'all',
-  tabWidth: 4,
-  semi: true,
-  singleQuote: true,
-  bracketSpacing: true,
-  printWidth: 100,
+    trailingComma: 'all',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    bracketSpacing: true,
+    printWidth: 100,
+    endOfLine: 'auto',
 };
